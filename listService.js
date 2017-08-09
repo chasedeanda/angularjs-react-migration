38b4601e563244a4f2fc3b46bc86d1ebe0cfced4
Chase Deanda <chasedeanda@gmail.com>
2017-08-09 09:52:40 -0500

    Initial commit

--- listService.js
+++
@@ -1,7 +0,0 @@
-angular.module('todo')
-
-.service('ListService', function() {
-  this.list = {};
-  this.getList = function() { return this.list; };
-  this.saveList = function(list) { this.list = list; };
-});
